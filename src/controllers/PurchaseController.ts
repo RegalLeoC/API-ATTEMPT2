8ce8c13141c2c3cbf5c5c0545300c43707e63a00
Adalbert047 <aadalberto835@gmail.com>
2023-11-15 07:59:11 -0600

    Create getPurchasebyId

--- src/controllers/PurchaseController.ts
+++ src/controllers/PurchaseController.ts
@@ -20,4 +20,26 @@ class PurchasController
             return res.status(500).json({ err: 'Internal Server Error'});
         }
     }
+
+    async getPurchaseById(req: Request, res:Response)
+    {
+        try
+        {
+            const purchaseId = parseInt(req.params.id, 10);
+            const purchaseRepository = getRepository(Purchase);
+            const purchase = await purchaseRepository.findOne( { where : {id : purchaseId}});
+
+            if(!purchase)
+            {
+                return res.status(404).json({ error: "Purchase Not Found"}) 
+            }
+            return res.json(purchase)
+        }
+        catch(err)
+        {
+            console.log(err);
+            return res.status(500).json({ err: 'Internal Server Error'});
+        }
+        
+    }
 }
