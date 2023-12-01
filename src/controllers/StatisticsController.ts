c8a07760891108529e2c1cdc106e037bc00dc2f8
Adalbert047 <aadalberto835@gmail.com>
2023-11-30 23:45:35 -0600

    Create StatisticsController

---
+++ src/controllers/StatisticsController.ts
@@ -0,0 +1,102 @@
+import { Request, Response } from "express";
+import { Statistics } from "../entities/Statistics";
+import { DeepPartial, getRepository } from "typeorm";
+// import new from './UserController';
+
+
+class StatisticsController 
+{
+
+    async getStatistics(req: Request, res : Response)
+    {
+        try
+        {
+            const statisticsRepository = getRepository(Statistics);
+            const statistics = await statisticsRepository.find();
+            return res.json(statistics);
+        }
+        catch(err)
+        {
+            console.log(err);
+            console.error('Error caught:', err);
+            return res.status(500).json({ err: 'Internal Server Error'});
+        }
+    }
+
+    async getStatisticsById(req: Request, res:Response)
+    {
+        try
+        {
+            // console.log("---------------------------------------------------");
+            // console.log("Purchase ID:", req.params.id, "Tipo:" , typeof req.params.id);
+            const statisticsId = parseInt(req.params.id, 10);
+            const statisticsRepository = getRepository(Statistics);
+
+
+            // if (isNaN(purchaseId)) {
+            //     console.log("Purchase ID:", purchaseId, "Tipo:" , typeof purchaseId);
+            //     return res.status(400).json({ error: 'Invalid purchase ID' });
+            //   }
+
+            const statistics = await statisticsRepository.findOne( { where : {id : statisticsId}});
+
+            if(!statistics)
+            {
+                return res.status(404).json({ error: "Statistic Not Found"}) 
+            }
+
+
+            return res.json(statistics)
+        }
+        catch(err)
+        {
+            console.log(err);
+            return res.status(500).json({ err: 'Internal Server Error PurchaseID'});
+        }
+    }
+
+    
+    async createStatistics(req: Request, res: Response)
+    {
+        try
+        {
+            const { purchase_id , product_id, revenue, quantity_sold } = req.body;
+
+
+            if (!purchase_id || !product_id || !revenue || !quantity_sold)
+            {
+                return res.status(400).json( {error: "Error, Empty Data"});
+            }
+            
+
+            //Verificar que si jale, ya que es un arreglo ahora
+            const statisticsRepository = getRepository(Statistics)
+            const partialStatisticArray = [
+               {
+                purchase_id, 
+                product_id, 
+                revenue, 
+                quantity_sold, 
+                date: new Date(),
+               },
+                
+              ];
+
+
+
+            const newStatistic = statisticsRepository.create(partialStatisticArray);
+            await statisticsRepository.save(newStatistic);
+
+
+            return res.status(201).json(newStatistic);
+        }
+        catch(err)
+        {
+            console.error('Error caught:', err);
+            return res.status(500).json({ err: 'Internal Server Error'});
+        }
+        
+    }
+}
+
+export default new StatisticsController();
