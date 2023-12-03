851e8abe10f236999e17eab23084fb8d526db556
Gabriela Estrello Guadarrama <gaby_estrello@hotmail.com>
2023-12-02 16:52:07 -0800

    Archivos funcionaels para el PromotionalProducts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { AppDataSource } from './index'
 import userRoutes from './routes/user.routes';
 import { User } from './entities/User';
+import PromotionalProductsRoutes from './routes/PromotionalProducts.routes'
+import { PromotionalProduct } from "./entities/PromotionalProduct";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +14,7 @@ AppDataSource.then(async(connection) => {
     console.log('Connected to supermarket Database')
 
     app.use('/api', userRoutes);
+    app.use('/api', PromotionalProductsRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`)
