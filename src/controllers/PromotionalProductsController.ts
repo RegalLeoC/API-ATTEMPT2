5283acfcc19886eb6bef3ef18589f74bcbdc474e
Victor Eduardo Garcia Sardaneta <regulusleoc@gmail.com>
2023-12-04 23:53:55 -0600

    PromotionalProducts Integrado Exitosamente

--- src/controllers/PromotionalProductsController.ts
+++ src/controllers/PromotionalProductsController.ts
@@ -34,32 +34,36 @@ class PromotionalProductController {
 
   async createPromotionalProduct(req: Request, res: Response) {
     try {
-      const { name, description, promotions_price, promotion_start_date, promotion_ending_date, promotion_hype } = req.body;
-
-      if (!name || !description || !promotions_price || !promotion_start_date || !promotion_ending_date || !promotion_hype) {
-        return res.status(400).json({ error: 'Name is required' });
+      const { name, description, promotions_price, promotion_start_date, promotion_ending_date, promotion_type, product_category, manufacturer, active } = req.body;
+  
+      if (!name || !description || !promotions_price || !promotion_start_date || !promotion_ending_date || active === undefined || !promotion_type) {
+        return res.status(400).json({ error: 'Name, description, promotions_price, promotion_start_date, promotion_ending_date, active, and promotion_type are required' });
       }
-
+  
       const promotionalProductRepository = getRepository(PromotionalProduct);
       const newPromotionalProduct = promotionalProductRepository.create({
         name,
         description,
-        promotions_price, // Note: In a real-world scenario, you should hash the password before saving it.
-        promotion_start_date: new Date(),
-        promotion_ending_date: new Date(),
-        active: 1,
-        promotion_hype, // Assuming 1 means active
+        promotions_price,
+        promotion_start_date: new Date(promotion_start_date),
+        promotion_ending_date: new Date(promotion_ending_date),
+        active: Boolean(active), // Ensure active is a boolean
+        promotion_type,
+        product_category,
+        manufacturer,
       });
-
+  
       await promotionalProductRepository.save(newPromotionalProduct);
-
+  
       return res.status(201).json(newPromotionalProduct);
-
+  
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
+  
+  
 
 
   async updatePromotionalProduct(req: Request, res: Response) {
@@ -67,28 +71,31 @@ class PromotionalProductController {
       const promotionalProductId = parseInt(req.params.id, 10);
       const promotionalProductRepository = getRepository(PromotionalProduct);
       const promotionalproduct = await promotionalProductRepository.findOne({ where: { id: promotionalProductId } });
-
+  
       if (!promotionalproduct) {
-        return res.status(404).json({ error: 'User not found' });
+        return res.status(404).json({ error: 'Promotional product not found' });
       }
-
-      // Update user properties based on your requirements
+  
+      // Update product properties based on your requirements
       promotionalproduct.name = req.body.name || promotionalproduct.name;
       promotionalproduct.description = req.body.description || promotionalproduct.description;
       promotionalproduct.promotions_price = req.body.promotions_price || promotionalproduct.promotions_price;
-      promotionalproduct.promotion_start_date = new Date();
-      promotionalproduct.promotion_ending_date = new Date();
-      promotionalproduct.promotion_hype = req.body.promotion_hype || promotionalproduct.promotion_hype;
-
+      promotionalproduct.promotion_start_date = new Date(req.body.promotion_start_date) || promotionalproduct.promotion_start_date;
+      promotionalproduct.promotion_ending_date = new Date(req.body.promotion_ending_date) || promotionalproduct.promotion_ending_date;
+      promotionalproduct.promotion_type = req.body.promotion_type || promotionalproduct.promotion_type;
+      promotionalproduct.product_category = req.body.product_category || promotionalproduct.product_category;
+      promotionalproduct.manufacturer = req.body.manufacturer || promotionalproduct.manufacturer;
+      promotionalproduct.active = Boolean(req.body.active); // Ensure active is a boolean
+  
       await promotionalProductRepository.save(promotionalproduct);
-
+  
       return res.json(promotionalproduct);
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
-
+  
   async deletePromotionalProduct(req: Request, res: Response) {
     try {
       const promotionalProductId = parseInt(req.params.id, 10);
@@ -101,7 +108,7 @@ class PromotionalProductController {
 
       await promotionalProductRepository.remove(promotionalproduct);
 
-      return res.json({ message: 'User deleted successfully' });
+      return res.json({ message: 'Promotional Product deleted successfully' });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: 'Internal Server Error' });
